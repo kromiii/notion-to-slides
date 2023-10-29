35c0ba86d7450fe60152aeab7c1abeea3c42b92b
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2023-10-29 22:47:21 +0900

    コマンドライン引数を受け取る

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+
 import { Greeter } from './Greeter'
+
+const args = process.argv.slice(2)
+const urlIndex = args.indexOf('--url')
+
+if (urlIndex === -1) {
+  console.log('Please provide a URL')
+  process.exit(1)
+}
+
+const url = args[urlIndex + 1]
 
 const greeter = new Greeter()
 
-greeter.greet('World')
+greeter.greet(url)
