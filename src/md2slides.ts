0f041e6b50f6e639ed3c827a95649fd728f0149b
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2023-11-01 09:03:11 +0900

    こっちを使う

--- src/md2slides.ts
+++ src/md2slides.ts
@@ -1,8 +1,30 @@
-import { Marpit } from '@marp-team/marpit'
+import { Marp } from '@marp-team/marp-core'
 import fs from 'fs'
 
 export default function md2slides(md: string): void {
-  const marpit = new Marpit()
+  const marpit = new Marp()
+  // const theme = `
+  // /* @theme example */
+
+  // section {
+  //   background-color: #369;
+  //   color: #fff;
+  //   font-size: 30px;
+  //   padding: 40px;
+  // }
+
+  // h1,
+  // h2 {
+  //   text-align: center;
+  //   margin: 0;
+  // }
+
+  // h1 {
+  //   color: #8cf;
+  // }
+  // `
+  // marpit.themeSet.default = marpit.themeSet.add(theme)
+
   const prefix = `
   <!--
 headingDivider: 2
