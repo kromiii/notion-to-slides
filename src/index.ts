744d639255483cf2ff331967f13d5314fda64280
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2023-11-05 10:39:12 +0900

    import opener

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import { getPageId } from './utils';
 import notion2md from './notion2md';
 import md2slides from './md2slides';
 
+const opener = require('opener');
+
 const pageId = getPageId();
 (async () => {
   const mdString = await notion2md(pageId);
   console.log(mdString);
   md2slides(mdString);
+  opener('example.html');
 })();
