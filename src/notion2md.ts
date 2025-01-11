d831de451948682d520317806f4abef49d3903bd
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2025-01-11 10:15:11 +0900

    prettier

--- src/notion2md.ts
+++ src/notion2md.ts
@@ -1,20 +1,22 @@
-import { NotionToMarkdown } from 'notion-to-md'
-import { Client } from '@notionhq/client'
+import { NotionToMarkdown } from "notion-to-md";
+import { Client } from "@notionhq/client";
 
-export default async function notion2md(pageId: string, token: string): Promise<string> {
+export default async function notion2md(
+  pageId: string,
+  token: string
+): Promise<string> {
   const notion = new Client({
-    auth: token
-  })
+    auth: token,
+  });
 
   const n2m = new NotionToMarkdown({
     notionClient: notion,
     config: {
       parseChildPages: false, // default: parseChildPages
-    }
+    },
   });
 
   const mdblocks = await n2m.pageToMarkdown(pageId);
   const mdString = await n2m.toMarkdownString(mdblocks);
   return mdString.parent;
-
 }
